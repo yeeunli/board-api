a676e0e9cd6afcf5b2d725131639dd3c40b9f69b
yeeunli <lye920@g.hongik.ac.kr>
2024-11-04 04:27:10 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/java/B_project/board_api/BoardApiApplication.java
@@ -0,0 +1,13 @@
+package B_project.board_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApiApplication.class, args);
+	}
+
+}
